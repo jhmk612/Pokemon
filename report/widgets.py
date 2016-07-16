b01424a00629a2a83a2e3a73a3fdd500fd171366
Minkyu Jheun <jhmk612@naver.com>
2016-07-17 02:00:16 +0900

    지도를 제외하고는 보고 형식 거의 다 갖춤

--- report/widgets.py
+++ report/widgets.py
@@ -7,7 +7,7 @@ class DaumMapWidget(forms.TextInput):
         lat, lng = 37.4624015, 126.9520365
 
         if value:
-            lat, lng = value.split(',')
+            lng, lat = value.split(',')
 
         html=super().render(name, value, attrs)
         rendered = render_to_string('daum_map_widget.html',
